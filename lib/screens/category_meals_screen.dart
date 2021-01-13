5bc4ebb996de850e6a19f867872ea8ea31c840a3
Sean Connolly <sconnolly17@gmail.com>
2021-01-12 21:35:10 -0700

    Added meal details 169

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals/dummy_data.dart';
+import 'package:flutter_meals/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
+    return Scaffold(
+        appBar: AppBar(title: Text(routeArgs['title'])),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+              id: categoryMeals[index].id,
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability,
+            );
+          },
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
